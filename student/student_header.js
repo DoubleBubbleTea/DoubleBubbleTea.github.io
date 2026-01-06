bfebd0c4c5e91427f7646514bae545e92fcefb62
Minh <minhnguyen2587@gmail.com>
2026-01-06 23:01:42 +0700

    Update fix modal icon

--- student/student_header.js
+++ student/student_header.js
@@ -147,23 +147,9 @@ function logout() {
 
 loadNotifications();
 
-function showPopup(title, message, type = "success") {
-    const icon = document.getElementById("popupIcon");
-
+function showPopup(title, message) {
     document.getElementById("modal-title").innerText = title;
     document.getElementById("modal-message").innerHTML = message;
-
-    // reset icon
-    icon.className = "popup-icon";
-
-    if (type === "error") {
-        icon.classList.add("error");
-        icon.innerText = "✕";
-    } else {
-        icon.classList.add("success");
-        icon.innerText = "✓";
-    }
-
     document.getElementById("popupModal").style.display = "flex";
 }
 
